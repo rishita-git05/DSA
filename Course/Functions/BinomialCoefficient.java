74c5d70191e6998bcc0b21eb38e782a0ff5745e7
rishita-git05 <rishitachalumuri@gmail.com>
2025-09-07 00:12:36 +0530

    Day 1: 06/09/2025 - Initial Commit

---
+++ Course/Functions/BinomialCoefficient.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class BinomialCoefficient 
+{
+    public static int factorial(int n)
+    {
+        int fact = 1;
+        for(int i = 1; i <= n; i++)
+            fact = fact * i;
+        return fact;
+    }
+
+    public static int binCoeff(int n, int r)
+    {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_n_r = factorial(n - r);
+        return fact_n / (fact_r * fact_n_r);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n and r:");
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+        System.out.println(binCoeff(n, r));
+        sc.close();
+    }
+}
---
+++ Course/Patterns/I/Character.java
@@ -0,0 +1,29 @@
+/*
+A
+BC
+DEF
+GHIJ
+*/
+
+import java.util.Scanner;
+
+public class Character
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no.of lines:");
+        int n = sc.nextInt();
+        char ch = 'A';
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
