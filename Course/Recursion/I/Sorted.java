567c27d1b551d5ee50cc0854ea63024ccfe9a2f8
rishita-git05 <rishitachalumuri@gmail.com>
2026-05-18 23:18:26 +0530

    Day 4: 18/05/2026

--- Course/Recursion/I/Sorted.java
+++ Course/Recursion/I/Sorted.java
@@ -2,9 +2,13 @@ import java.util.*;
 
 public class Sorted 
 {
-    static boolean isSorted(int[] arr)
+    static boolean isSorted(int[] arr, int i)
     {
-        
+        if (i == arr.length - 1)
+            return true;
+        if (arr[i] > arr[i + 1])
+            return false;
+        return isSorted(arr, i + 1);
     }
     public static void main(String[] args)
     {
@@ -13,6 +17,7 @@ public class Sorted
         int[] arr = new int[n];
         for(int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
-
+        System.out.println(isSorted(arr, 0));
+        sc.close();
     }
 }
